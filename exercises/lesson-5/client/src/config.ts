a399217648bb4efcfe58695fdd084327575fb5e3
jinjinlei2108 <Mofaxvetu1>
2023-02-11 14:27:31 +1100

    Lesson 5 Auth stage

--- exercises/lesson-5/client/src/config.ts
+++ exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://xobhvcu0c7.execute-api.us-east-1.amazonaws.com/dev/'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-dt5w73xr4ennm0m8.us.auth0.com',
+  clientId: 'jOODoMdMnhKIAlkGtprcbMq0QroX3DvV',
   callbackUrl: 'http://localhost:3000/callback'
 }
