06b826e27cde66e81ea58210816f3e97f957307a
jinjinlei2108 <Mofaxvetu1>
2023-02-11 14:30:37 +1100

    Lesson 5 Auth stage

---
+++ exercises/c4-demos-master/10-udagram-app-follow-alongversion/src/lambda/s3/resizeImage.ts
@@ -0,0 +1,45 @@
+import { SNSEvent, SNSHandler, S3EventRecord } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+import Jimp from 'jimp/es'
+
+const s3 = new AWS.S3()
+
+
+const IMAGES_S3_BUCKET= process.env.IMAGES_S3_BUCKET
+const THUMBNAILS_S3_BUCKET= process.env.THUMBNAILS_S3_BUCKET
+
+export const handler: SNSHandler = async (event: SNSEvent) => {
+    console.log('Processing SNS event ', JSON.stringify(event))
+    for (const snsRecord of event.Records){
+        const s3EventStr =snsRecord.Sns.Message
+        console.log('Processing S3 event', s3EventStr)
+        const s3Event= JSON.parse(s3EventStr)
+        
+        for (const record of s3Event.Records)
+        await processImage(record)
+    }
+}
+
+
+async function processImage(record:S3EventRecord) {
+    const key = record.s3.object.key
+    const response = await s3.getObject({
+        Bucket: IMAGES_S3_BUCKET,
+        Key: key
+    })
+    .promise()
+    const body_Buffer = response.Body as Buffer
+    const image = await Jimp.read(body_Buffer)
+    image.resize(150, Jimp.AUTO)
+    const convertedBuffer = await image.getBufferAsync(Jimp.AUTO)
+
+    await s3.putObject({
+        Bucket: THUMBNAILS_S3_BUCKET,
+        Key: `${key}.jpeg`,
+        Body: convertedBuffer
+    })
+    .promise()
+
+
+}
--- exercises/c4-demos-master/10-udagram-app-follow-alongversion/src/lambda/s3/sendNotifications.ts
+++ exercises/c4-demos-master/10-udagram-app-follow-alongversion/src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import { S3Handler, S3Event } from "aws-lambda";
+import { SNSEvent, S3Event, SNSHandler } from "aws-lambda";
 import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
 
@@ -11,9 +11,20 @@ const connectionParams = {
     endpoint: `${apiId}.execute-api.us-east-1.amazonaws.com/${stage}`
 }
 
+
+export const handler: SNSHandler= async (event: SNSEvent) => {
+    console.log("Processing SNS event", JSON.stringify(event))
+    for (const snsRecord of event.Records){
+        const s3EventStr = snsRecord.Sns.Message
+        console.log('Processing S3 event', s3EventStr)
+        const s3Event = JSON.parse(s3EventStr)
+        await processS3Event(s3Event)
+    }
+}
+
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
-export const handler: S3Handler = async (event: S3Event) => {
-    for (const record of event.Records) {
+async function processS3Event(s3event: S3Event){
+    for (const record of s3event.Records) {
         const key = record.s3.object.key
         console.log('Processiong S3 item with key: ', key)
 
